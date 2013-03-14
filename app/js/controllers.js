3830eb66cf3c0af168fbba19c1213e14ec51b483
Brandon Goh <kwgoh.2010@sis.smu.edu.sg>
2013-03-14 18:00:50 +0800

    Songs and Challenges updated. Have to fix encoding for Japanese and special characters.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,7 +17,30 @@ function FirstController($scope,$resource) {
 	/* Login */					
 	$scope.login = {};
 	$scope.login.data = {"u_name":"","u_pwd":""};
-	               
+
+	$scope.retrievesession = function() {
+		if (!localStorage.getItem('u_name') || !localStorage.getItem('u_admin')) {
+			localStorage.removeItem('u_name');
+			localStorage.removeItem('u_admin');		
+			$scope.name = "BAKA";
+			$scope.isadmin = "";		
+		} else {
+			$scope.name = localStorage.getItem('u_name');
+			if (localStorage.getItem('u_admin') === "true") {
+				$scope.isadmin = true;
+			} else {
+				$scope.isadmin = false;
+			}
+		}
+	}
+	
+	$scope.logout = function() {
+		localStorage.removeItem('u_name');
+		localStorage.removeItem('u_admin');
+		$scope.name = "BAKA";
+		$scope.isadmin = "";
+	}
+	
 	$scope.getlogin = function(){
 	  $scope.LoadLogin = $resource('http://:remote_url/login/login', 
 					{"remote_url":$scope.remote_url},                       {'save': { method: 'POST',    params: {} }});
@@ -26,8 +49,15 @@ function FirstController($scope,$resource) {
 	  var login = new $scope.LoadLogin($scope.login.data);
 	  login.$save(function(response) { 
 			  var result = response;
-			  $scope.name = result.u_name;
-			  $scope.isadmin = result.u_admin;
+			  if (result.error === undefined) {
+				$scope.name = result.u_name;
+				$scope.isadmin = result.u_admin;
+				localStorage.setItem('u_name', $scope.name);
+				localStorage.setItem('u_admin', $scope.isadmin);
+			  } else {
+				alert(result.error);
+			  }
+			  $scope.login.data = {"u_name":"","u_pwd":""};
 			  $scope.waiting = "Ready";
 			}); 
 	};
@@ -40,8 +70,15 @@ function FirstController($scope,$resource) {
 	  var login = new $scope.SaveLogin($scope.login.data);
 	  login.$save(function(response) { 
 			  var result = response;
-			  $scope.name = result.u_name;
-			  $scope.isadmin = result.u_admin;
+			  if (result.error === undefined) {
+				$scope.name = result.u_name;
+				$scope.isadmin = result.u_admin;
+				localStorage.setItem('u_name', $scope.name);
+				localStorage.setItem('u_admin', $scope.isadmin);
+			  } else {
+				alert(result.error);
+			  }
+			  $scope.login.data = {"u_name":"","u_pwd":""};
 			  $scope.waiting = "Ready";
 			}); 
 	};	
@@ -49,8 +86,20 @@ function FirstController($scope,$resource) {
 	/* Challenge (Admins) */
 					
 	$scope.challenge = {};
-	$scope.challenge.data = {"c_name":"","c_desc":"","c_hint":"","c_points":"0","c_notes":"","c_blob":""};
+	$scope.challenge.data = {"c_name":"","c_desc":"","c_date":"","c_songs":[],"c_notes":"","c_blob":""};
 	
+    $scope.c_song = {"s_name":"","s_difficulty":""};
+    
+    $scope.addsongtochallenge = function() {
+        $scope.challenge.data.c_songs.push($scope.c_song);
+        $scope.c_song = {"s_name":"","s_difficulty":""};
+    }
+    
+    $scope.cancelchallenge = function() {
+        $scope.challenge.data = {"c_name":"","c_desc":"","c_date":"","c_songs":[],"c_notes":"","c_blob":""};    
+        $scope.c_song = {"s_name":"","s_difficulty":""};
+    }
+    
 	$scope.addchallenge = function(){
 	  $scope.SaveChallenge = $resource('http://:remote_url/challenge/create', 
 					{"remote_url":$scope.remote_url},                       {'save': { method: 'POST',    params: {} }});
@@ -59,6 +108,7 @@ function FirstController($scope,$resource) {
 	  var challenge = new $scope.SaveChallenge($scope.challenge.data);
 	  challenge.$save(function(response) { 
 			  var result = response;
+			  $scope.challenge.data = {"c_name":"","c_desc":"","c_date":"","c_songs":[],"c_notes":"","c_blob":""};
 			  $scope.listchallenge();
 			  $scope.waiting = "Ready";
 			}); 
@@ -75,7 +125,41 @@ function FirstController($scope,$resource) {
 			  $scope.waiting = "Ready";
 			});  
 	};
+	
+	/* Songs (Admins) */
+	
+	$scope.song = {};
+	$scope.song.data = {"s_name":"","s_comp":"","s_details":"","s_uploadby":""};
+	
+ 
+	$scope.listsongs = function(){
+	  $scope.LoadAllSongs = $resource('http://:remote_url/songdata/list', 
+					{"remote_url":$scope.remote_url},
+					{'get': {method: 'JSONP', isArray: false, params:{callback: 'JSON_CALLBACK'}}});
+					
+	  $scope.waiting = "Updating";       
+	  $scope.LoadAllSongs.get(function(response) { 
+			  $scope.songs = response;
+			  $scope.waiting = "Ready";
+			});  
+	};
+	
+    /* File Submissions (Admins) */
+    
+	$scope.listsubmissions = function(){
+	  $scope.LoadAllFiles = $resource('http://:remote_url/file/list', 
+					{"remote_url":$scope.remote_url},
+					{'get': {method: 'JSONP', isArray: false, params:{callback: 'JSON_CALLBACK'}}});
+					
+	  $scope.waiting = "Updating";       
+	  $scope.LoadAllFiles.get(function(response) { 
+			  $scope.blobfiles = response;
+			  $scope.waiting = "Ready";
+			});  
+	};
 
+	$scope.retrievesession();
 	$scope.listchallenge();
+	$scope.listsongs();
 }
 
--- blob.py
+++ blob.py
@@ -22,7 +22,68 @@ class BlobData(db.Model):
   file_date = db.DateTimeProperty(auto_now_add=True)
   file_owner = db.StringProperty()
   file_blob_key = db.StringProperty(required=True)
+  
+  def to_dict(self):
+       d = dict([(p, unicode(getattr(self, p))) for p in self.properties()])
+       d["id"] = self.key().id()
+       return d
 
+            
+  @staticmethod
+  def get_entities():
+    #update ModelCount when adding
+    theQuery = BlobData.all()
+
+    objects = theQuery.run()
+
+    entities = []
+    for object in objects:
+      entity = {'f_name': object.file_name,
+              'f_desc': object.file_desc,
+              'f_date': object.file_date,
+              'f_owner': object.file_owner,
+              'f_blob': object.file_blob_key}
+      entities.append(entity)
+    result = {'method':'get_entities',
+              'en_type': 'Blob',
+              'entities': entities}       
+    return result  
+
+class SongData(db.Model):
+  song_name = db.StringProperty(required=True,default='Unknown')
+  song_composer = db.StringProperty(required=True,default='Unknown')
+  song_details = db.StringProperty()
+  song_date = db.DateTimeProperty(auto_now_add=True)
+  song_upload_by = db.StringProperty()
+  song_blob_key = db.StringProperty(required=True)
+  
+  def to_dict(self):
+       d = dict([(p, unicode(getattr(self, p))) for p in self.properties()])
+       d["id"] = self.key().id()
+       return d
+
+            
+  @staticmethod
+  def get_entities():
+    #update ModelCount when adding
+    theQuery = SongData.all()
+
+    objects = theQuery.run()
+
+    entities = []
+    for object in objects:
+      entity = {'s_name': object.song_name,
+              's_comp': object.song_composer,
+              's_details': object.song_details,
+              's_date': object.song_date,
+              's_uploadby': object.song_upload_by,
+              's_blob': object.song_blob_key}
+      entities.append(entity)
+    result = {'method':'get_entities',
+              'en_type': 'Song',
+              'entities': entities}       
+    return result
+    
 class User(db.Model):
   user_name = db.StringProperty(required=True)
   user_password = db.StringProperty(required=True)
@@ -47,7 +108,6 @@ class User(db.Model):
     entity.put()
     
     result = {'u_name':jsonData['u_name'],
-              'u_pwd': jsonData['u_pwd'],
               'u_admin': bool(False)}
         
     return result
@@ -57,7 +117,6 @@ class User(db.Model):
     theobject = User.get_by_id(int(model_id))
     
     result = {'u_name':theobject.user_name,
-              'u_pwd': theobject.user_pwd,
               'u_admin': theobject.user_isadmin}
     return result
     
@@ -107,15 +166,17 @@ class User(db.Model):
               }
     return result
 
+class Challenge_Songs(db.Model):
+  challenge_id = db.IntegerProperty(required=True,default=0)
+  song_name = db.StringProperty(required=True,default="")
+  difficulty = db.StringProperty(required=True,default="Basic")
+    
 class Challenge(db.Model):
   challenge_name = db.StringProperty(required=True)
   challenge_description = db.StringProperty()
-  challenge_hint = db.StringProperty()
-  challenge_points = db.IntegerProperty()
   challenge_admin_notes = db.StringProperty()
   challenge_date = db.DateTimeProperty(auto_now_add=True)
   challenge_expiry = db.DateTimeProperty()
-  challenge_blob_key = db.StringProperty()
   
   def to_dict(self):
        d = dict([(p, unicode(getattr(self, p))) for p in self.properties()])
@@ -129,37 +190,59 @@ class Challenge(db.Model):
     jsonData = json.loads(data)
     entity = Challenge(challenge_name=jsonData['c_name'],
                     challenge_description=jsonData['c_desc'],
-                    challenge_hint=jsonData['c_hint'],
-                    challenge_points=int(jsonData['c_points']),
                     challenge_admin_notes=jsonData['c_notes'],
-                    challenge_blob_key=jsonData['c_blob'],)
+                    challenge_expiry=jsonData['c_date'])
     
     entity.put()
     
-    result = {'c_name':jsonData['c_name'],
+    challengeSongs = jsonData['c_songs'];
+    count = 0
+    for i in range(len(challengeSongs)):
+      csData = challengeSongs[i]
+      csong = Challenge_Songs(challenge_id=entity.key().id(),
+                    song_name=csData['s_name'],
+                    difficulty=csData['s_difficulty'])
+      csong.put()
+      count = count + 1
+    result = {'c_name': jsonData['c_name'],
               'c_desc': jsonData['c_desc'],
-              'c_hint': jsonData['c_hint'],
-              'c_points': jsonData['c_points'],
               'c_notes': jsonData['c_notes'],
-              'c_blob': jsonData['c_blob']}
+              'c_date': jsonData['c_date'],
+              'c_blob': jsonData['c_blob'],
+              'c_songs_count': count}
         
     return result
   
   @staticmethod
   def get_entities():
     #update ModelCount when adding
+
     theQuery = Challenge.all()
 
     objects = theQuery.run()
 
     entities = []
     for object in objects:
+      c_songs_query = Challenge_Songs.all().filter('challenge_id', object.key().id()).run()
+      
+      c_songs = []
+      
+      if c_songs_query:
+        for c_s in c_songs_query:
+          c_song_blob = ""
+          c_song_data = SongData.all().filter('song_name',c_s.song_name).get()
+          if c_song_data:
+            c_song_blob = c_song_data.song_blob_key
+          c_entity = {'s_name': c_s.song_name,
+                's_difficulty': c_s.difficulty,
+                's_blob': c_song_blob}
+          c_songs.append(c_entity)
+      
       entity = {'c_name': object.challenge_name,
               'c_desc': object.challenge_description,
-              'c_hint': object.challenge_hint,
-              'c_points': object.challenge_points,
               'c_notes': object.challenge_admin_notes,
-              'c_blob': object.challenge_blob_key}
+              'c_date': object.challenge_expiry,
+              'c_songs': c_songs}
       entities.append(entity)
     result = {'method':'get_entities',
               'en_type': 'Challenge',
@@ -170,12 +253,26 @@ class Challenge(db.Model):
   def get_entity(model_id):
     theobject = Challenge.get_by_id(int(model_id))
     
+    c_songs_query = Challenge_Songs.all().filter('challenge_id', theobject.key().id()).run()
+    
+    c_songs = []
+    
+    if c_songs_query:
+      for c_s in c_songs_query:
+        c_song_blob = ""
+        c_song_data = SongData.all().filter('song_name',c_s.song_name).get()
+        if c_song_data:
+          c_song_blob = c_song_data.song_blob_key
+        c_entity = {'s_name': c_s.song_name,
+              's_difficulty': c_s.difficulty,
+              's_blob': c_song_blob}
+        c_songs.append(c_entity)
+      
     result = {'c_name': theobject.challenge_name,
               'c_desc': theobject.challenge_description,
-              'c_hint': theobject.challenge_hint,
-              'c_points': theobject.challenge_points,
               'c_notes': theobject.challenge_admin_notes,
-              'c_blob': theobject.challenge_blob_key}
+              'c_date': object.challenge_expiry,
+              'c_songs': c_songs}
     return result
     
   @staticmethod
@@ -215,10 +312,8 @@ class Challenge(db.Model):
       entity.challenge_name=jsonData['c_name']
     if jsonData['c_desc']!='':
       entity.challenge_description=jsonData['c_desc']
-    if jsonData['c_hint']!='':
-      entity.challenge_hint=jsonData['c_hint']
-    if jsonData['c_points']!='':
-      entity.challenge_points=int(jsonData['c_points'])
+    if jsonData['c_date']!='':
+      entity.challenge_expiry=jsonData['c_date']
     if jsonData['c_notes']!='':
       entity.challenge_admin_notes=jsonData['c_notes']
     if jsonData['c_blob']!='':
@@ -240,7 +335,7 @@ class BlobFormHandler(webapp2.RequestHandler):
     self.response.out.write('<script src="app/js/app.js"></script>')
     self.response.out.write('<script src="test/lib/angular/angular-mocks.js"></script>')
     self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-    self.response.out.write("""Name of file:<br> <input type="text" name="filename"><br> Description:<br> <input type="text" name="filedescription"><br><hr/> Upload File: <input type="file" name="file"><br> <input type="hidden" name="fileowner" ng-model="name"> <input type="submit" name="submit" value="Submit"> </form></body></html>""")
+    self.response.out.write("""Name of file:<br> <input type="text" name="filename"><br> Description:<br> <input type="text" name="filedescription"><br><hr/> Upload File: <input type="file" name="file"><br> Uploaded By:<br> <input type="text" name="fileowner" ng-model="name" disabled> <input type="submit" name="submit" value="Submit"> </form></body></html>""")
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
   def post(self):
@@ -270,31 +365,65 @@ class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
     self.send_blob(blob_info)
 
 class ListBlobHandler(webapp2.RequestHandler):
+  def respond(self,result):
+    """Returns a JSON response to the client.
+    """
+    callback = self.request.get('callback')
+    self.response.headers['Content-Type'] = 'application/json'
+    #self.response.headers['Content-Type'] = '%s; charset=%s' % (config.CONTENT_TYPE, config.CHARSET)
+    self.response.headers['Access-Control-Allow-Origin'] = '*'
+    self.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD'
+    self.response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, X-Requested-With'
+    self.response.headers['Access-Control-Allow-Credentials'] = 'True'
+
+    #Add a handler to automatically convert datetimes to ISO 8601 strings. 
+    dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime) else None
+    if callback:
+      content = str(callback) + '(' + json.dumps(result,default=dthandler) + ')'
+      return self.response.out.write(content)
+        
+    return self.response.out.write(json.dumps(result,default=dthandler))
+        
   def get(self):
-    # Query interface constructs a query using instance methods
-    q = BlobData.all()
+    result = BlobData.get_entities()
+    
+    return self.respond(result)
+
+class SongFormHandler(webapp2.RequestHandler):
+  def get(self):
+    upload_url = blobstore.create_upload_url('/file/songupload')
+    self.response.out.write('<html ng-app="myApp"><body ng-controller="FirstController"><link href="app/css/bootstrap.css" rel="stylesheet">')
+    self.response.out.write('<script src="app/lib/angular/angular.min.js"></script>')
+    self.response.out.write('<script src="app/lib/angular/angular-resource.min.js"></script>')
+    self.response.out.write('<script src="app/js/controllers.js"></script>')
+    self.response.out.write('<script src="app/js/app.js"></script>')
+    self.response.out.write('<script src="test/lib/angular/angular-mocks.js"></script>')
+    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+    self.response.out.write("""Name of Song<br> <input type="text" name="s_name"><br> Composer<br> <input type="text" name="s_comp"><br> Song Details<br> <input type="text" name="s_details"><br> Uploaded By<br> <input type="text" name="s_uploadby" ng-model="name" disabled><br> Upload File: <input type="file" name="song"><br><input type="submit" name="submit" value="Submit"> </form></body></html>""")
 
 
-    # GqlQuery interface constructs a query using a GQL query string
-    q = db.GqlQuery("SELECT * FROM BlobData")
-
-
-    self.response.out.write('<html><body>')
-    self.response.out.write('<table>')
-    self.response.out.write('<thead></thead>')
-    self.response.out.write('<tbody>')
-    # Query is not executed until results are accessed
-    for p in q.run():
-      self.response.out.write('<tr>')
-      self.response.out.write('<td>%s</td>' % p.file_name)
-      self.response.out.write('<td>%s</td>' % p.file_desc)
-      self.response.out.write('<td>%s</td>' % p.file_owner)
-      self.response.out.write('<td><a href="/file/serve/%s">Link</a></td>' % p.file_blob_key)
-      self.response.out.write('</tr>')
-
-    self.response.out.write('</tbody>')
-    self.response.out.write('<table>')
-    self.response.out.write('</body></html>')
+class SongUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+  def post(self):
+    upload_song = self.get_uploads('song')  # 'file' is file upload field in the form
+    s_name = self.request.POST.get('s_name')
+    s_comp = self.request.POST.get('s_comp')
+    s_details = self.request.POST.get('s_details')
+    s_uploadby = self.request.POST.get('s_uploadby')
+    blob_info = upload_song[0]
+    
+    try:
+        s = SongData(song_name=s_name,
+             song_composer=s_comp,
+             song_details=s_details,
+             song_upload_by=s_uploadby,
+             song_blob_key=str(blob_info.key()))
+        s.date = datetime.datetime.now().date()
+        s.put()
+    
+        #self.redirect('/file/serve/%s' % blob_info.key())
+    except BadValueError:
+        print "Error in uploading song!"
+    #$self.response.out.write(blob_info.key());
 
 class LoginHandler(webapp2.RequestHandler):
   def respond(self,result):
@@ -321,13 +450,11 @@ class LoginHandler(webapp2.RequestHandler):
     # Query interface constructs a query using instance methods
     q = User.all()
     objects = q.run()
-    result = {'u_name':'Invalid Username/Password',
-              'u_admin':'Password!'}
+    result = {'error':'Invalid Username/Password!'}  
     for object in objects:
       if jsonData['u_name']==object.user_name:
         if jsonData['u_pwd']==object.user_password:
           result = {'u_name':object.user_name,
-              'u_pwd': object.user_password,
               'u_admin': object.user_isadmin}
               
     return self.respond(result)
@@ -359,8 +486,7 @@ class RegisterHandler(webapp2.RequestHandler):
     objects = q.run()
     
     valid = 1
-    result = {'u_name':'Invalid Username/Password',
-              'u_admin':'Password!'}    
+    result = {'error':'Username already exists!'}    
     for object in objects:
       if jsonData['u_name']==object.user_name:
         valid = 0
@@ -421,13 +547,41 @@ class ListChallengeHandler(webapp2.RequestHandler):
     
     return self.respond(result)
     
+class ListSongsHandler(webapp2.RequestHandler):
+  def respond(self,result):
+    """Returns a JSON response to the client.
+    """
+    callback = self.request.get('callback')
+    self.response.headers['Content-Type'] = 'application/json'
+    #self.response.headers['Content-Type'] = '%s; charset=%s' % (config.CONTENT_TYPE, config.CHARSET)
+    self.response.headers['Access-Control-Allow-Origin'] = '*'
+    self.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD'
+    self.response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, X-Requested-With'
+    self.response.headers['Access-Control-Allow-Credentials'] = 'True'
+
+    #Add a handler to automatically convert datetimes to ISO 8601 strings. 
+    dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime) else None
+    if callback:
+      content = str(callback) + '(' + json.dumps(result,default=dthandler) + ')'
+      return self.response.out.write(content)
+        
+    return self.response.out.write(json.dumps(result,default=dthandler))
+        
+  def get(self):
+    result = SongData.get_entities()
+    
+    return self.respond(result)
+    
 app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/blobform', BlobFormHandler),
                                ('/file/upload', UploadHandler),
+                               ('/songform', SongFormHandler),
+                               ('/file/songupload', SongUploadHandler),
                                ('/file/serve/([^/]+)?', ServeHandler),
                                ('/file/list', ListBlobHandler),
                                ('/login/login', LoginHandler),
                                ('/login/register', RegisterHandler),
                                ('/challenge/create', CreateChallengeHandler),
-                               ('/challenge/list', ListChallengeHandler)],
+                               ('/challenge/list', ListChallengeHandler),
+                               ('/songdata/list', ListSongsHandler)],
                               debug=True)
